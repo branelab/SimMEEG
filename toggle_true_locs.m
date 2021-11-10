d88d3fe37acfe2f42cef568abf875f370248de67
branelab <77899096+branelab@users.noreply.github.com>
2021-11-09 17:08:45 -0800

    Add files via upload

---
+++ toggle_true_locs.m
@@ -0,0 +1,10 @@
+function toggle_true_locs(varargin)
+global h
+for v=1:3
+if h.radio_3D_true_locs.Value == 1
+    set(handle(h.map3D_true_locs(1,v)),'Visible','on'); set(handle(h.map3D_true_locs(2,v)),'Visible','on');
+elseif h.radio_3D_true_locs.Value == 0
+    set(handle(h.map3D_true_locs(1,v)),'Visible','off'); set(handle(h.map3D_true_locs(2,v)),'Visible','off');
+end
+end
+bs_plot_peak_waves;
